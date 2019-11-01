b1434c30a4327987189521d32913c5abafbe1126
PepoVulshebnika <krenworld@abv.bg>
2019-11-01 21:26:10 +0200

    -creating login page -creating main page -creating product page -creating card page -creating reference page -creating user page

--- src/main/java/bg/sit/ui/MainApp.java
+++ src/main/java/bg/sit/ui/MainApp.java
@@ -11,26 +11,20 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
-
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-
-        stage.setTitle("X.Inventory.Management");
-        stage.setScene(scene);
-        stage.show();
+   @Override
+    public void start(Stage primaryStage){
+        try{
+                Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginPage.fxml"));
+                Scene scene = new Scene(root);
+                scene.getStylesheets().add(getClass().getResource("/styles/application.css").toExternalForm());
+                primaryStage.setTitle("X.Inventory.Management");
+                primaryStage.setScene(scene);
+                primaryStage.show();
+        }   catch(Exception e){
+            e.printStackTrace();
+        }   
     }
-
-    /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         launch(args);
     }
---
+++ src/main/java/bg/sit/ui/controllers/LoginPageController.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bg.sit.ui.controllers;
+
+import bg.sit.business.services.UsersService;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author perel
+ */
+public class LoginPageController implements Initializable {
+    
+        UsersService usersService;
+        @FXML
+	private AnchorPane rootPane;
+	@FXML
+	private TextField txtUsernameField ;
+	@FXML
+        private TextField txtPasswordField;
+        @FXML
+	private Label lblStatus;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    
+    }    
+    
+    public void btnLogin(ActionEvent event) throws Exception {
+        usersService = new UsersService();
+    if (usersService.login(txtUsernameField.getText(), txtPasswordField.getText())) {
+        
+                AnchorPane pane =FXMLLoader.load(getClass().getResource("/fxml/MainPage.fxml"));
+		rootPane.getChildren().setAll(pane);
+                
+		} else {
+                lblStatus.setText("Login Failed");
+        }
+    }
+    
+}
