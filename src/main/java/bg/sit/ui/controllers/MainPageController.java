cbd18c1379a8e1ab897616063b8eb31287c78b58
Borislav Nikolov <borislav.nikolov@petroffsoft.bg>
2019-11-04 16:07:09 +0200

    Added SessionHelper, Customer and CustomersService, made the relation between User and Customer

--- src/main/java/bg/sit/ui/controllers/MainPageController.java
+++ src/main/java/bg/sit/ui/controllers/MainPageController.java
@@ -1,15 +1,60 @@
 package bg.sit.ui.controllers;
 
-import bg.sit.business.services.UsersService;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 
 public class MainPageController implements Initializable {
 
+    @FXML
+    private AnchorPane rootPane;
+    @FXML
+    private Label UserName;
+    @FXML
+    private Label N_Products;
+    @FXML
+    private Label N_Clients;
+    @FXML
+    private Label N_Cards;
+    @FXML
+    private Label N_Users;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
+        
+    }    
+     public void Page_Products(ActionEvent event) throws IOException {
+        AnchorPane pane =FXMLLoader.load(getClass().getResource("/fxml/Page_Products.fxml"));
+	rootPane.getChildren().setAll(pane);
     }
-
+    public void Page_Clients(ActionEvent event) throws IOException {
+        AnchorPane pane =FXMLLoader.load(getClass().getResource("/fxml/Page_Clients.fxml"));
+	rootPane.getChildren().setAll(pane);
+    }
+    public void Page_Cards(ActionEvent event) throws IOException {
+        AnchorPane pane =FXMLLoader.load(getClass().getResource("/fxml/Page_Cards.fxml"));
+	rootPane.getChildren().setAll(pane);
+    }
+    public void Page_Reference(ActionEvent event) throws IOException {
+        AnchorPane pane =FXMLLoader.load(getClass().getResource("/fxml/Page_Reference.fxml"));
+	rootPane.getChildren().setAll(pane);
+    }
+    public void Page_Users(ActionEvent event) throws IOException {
+        AnchorPane pane =FXMLLoader.load(getClass().getResource("/fxml/Page_Users.fxml"));
+	rootPane.getChildren().setAll(pane);
+    }
+    public void LogOut(ActionEvent event) throws IOException {
+        AnchorPane pane =FXMLLoader.load(getClass().getResource("/fxml/LoginPage.fxml"));
+	rootPane.getChildren().setAll(pane);
+    }
+    public void Exit(ActionEvent event) throws IOException {
+        System.exit(0);
+    }
 }
---
+++ src/main/java/bg/sit/ui/controllers/Page_ProductsController.java
@@ -0,0 +1,27 @@
+package bg.sit.ui.controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+
+public class Page_ProductsController implements Initializable {
+
+    @FXML
+    private AnchorPane rootPane;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    public void GoBack(ActionEvent event) throws IOException {
+        AnchorPane pane =FXMLLoader.load(getClass().getResource("/fxml/MainPage.fxml"));
+	rootPane.getChildren().setAll(pane);
+    } 
+    
+}
