081645394e529c96638d0b05cfcff86520d75c7e
Sesi0 <pandafreakbg@gmail.com>
2019-11-11 16:03:04 +0200

    Added bootstrap style for the application, fixed closing the app was not killing threads

--- src/main/java/bg/sit/ui/MainApp.java
+++ src/main/java/bg/sit/ui/MainApp.java
@@ -2,10 +2,13 @@ package bg.sit.ui;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class MainApp extends Application {
 
@@ -15,9 +18,16 @@ public class MainApp extends Application {
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/LoginPage.fxml"));
             Scene scene = new Scene(root);
             scene.getStylesheets().add(getClass().getResource("/styles/application.css").toExternalForm());
+
             primaryStage.setTitle("X.Inventory.Management");
             primaryStage.setScene(scene);
-            primaryStage.resizableProperty().setValue(false);
+            primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+                @Override
+                public void handle(WindowEvent t) {
+                    Platform.exit();
+                    System.exit(0);
+                }
+            });
             primaryStage.show();
 
         } catch (Exception e) {
