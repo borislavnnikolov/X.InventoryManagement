7ab39b1e89bb0136cd209e1b4cab560e8ddbd5ea
PepoVulshebnika <krenworld@abv.bg>
2019-11-11 02:43:20 +0200

    - litreally nothing

--- src/main/java/bg/sit/ui/MainApp.java
+++ src/main/java/bg/sit/ui/MainApp.java
@@ -6,10 +6,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class MainApp extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
         try {
@@ -19,14 +18,13 @@ public class MainApp extends Application {
             primaryStage.setTitle("X.Inventory.Management");
             primaryStage.setScene(scene);
             primaryStage.resizableProperty().setValue(false);
-            primaryStage.initStyle(StageStyle.TRANSPARENT);
             primaryStage.show();
-            
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
